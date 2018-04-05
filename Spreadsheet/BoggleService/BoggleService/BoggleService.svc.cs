3f6c6d9335cc579cdf32209e8d841eb275dc683e
Ocmersh <Ocmersh@gmail.com>
2018-04-05 11:34:25 -0600

    Add unit tests

--- Spreadsheet/BoggleService/BoggleService/BoggleService.svc.cs
+++ Spreadsheet/BoggleService/BoggleService/BoggleService.svc.cs
@@ -12,7 +12,7 @@ using static System.Net.HttpStatusCode;
 /// This namespace acts as the logic for a server that users can connect to, in order to play other users in a Bogglegame. It implements
 /// IBoggleService, and uses BoggleBoard for the board layout.
 /// 
-/// Authors: Bryce Hansen, Chris Hanson
+/// Authors: Bryce Hansen, Chris Hanson.
 /// </summary>
 namespace Boggle
 {
